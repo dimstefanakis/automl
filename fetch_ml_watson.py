c4bd50922fee01f8c271f2acbe14b0cb8cd23391
Dimitris Stefanakis <jimstef@outlook.com>
2022-01-28 18:36:53 +0200

    a

--- fetch_ml_watson.py
+++ fetch_ml_watson.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 import Features, ClassificationsOptions
+from ibm_watson.natural_language_understanding_v1 import Features, ClassificationsOptions, CategoriesOptions, KeywordsOptions
 load_dotenv()
 
 # Add your NLU credentials here
@@ -18,9 +18,12 @@ nlu.set_service_url(url)
 
 print("Successfully connected with the NLU service")
 
-text = "When i squat the right side of my hip is always slightly higher than my left even if use less weights, what are some ways to fix this?"
+text = "How can I get parent state for child pages in Next.js?"
 
 analysis = nlu.analyze(text=text, features=Features(classifications=ClassificationsOptions(model=model_id))).get_result()
+response = nlu.analyze(
+    text=text,
+    features=Features(keywords=KeywordsOptions(limit=10))).get_result()
 
 print("Analysis response from trained NLU Classifications model:")
 print(json.dumps(analysis, indent=4))
